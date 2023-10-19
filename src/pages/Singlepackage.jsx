7f31dd16bd0a37cdd1ea113c2e566fc84b323020
9mmasi <67147238+9mmasi@users.noreply.github.com>
2023-10-19 14:42:21 +0000

    Update Singlepackage.jsx

--- src/pages/Singlepackage.jsx
+++ src/pages/Singlepackage.jsx
@@ -2,63 +2,50 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useGlobalContext } from '../../Context';
 import Hero from '../components/Hero';
+import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
+import './single.css';
 
 const Singlepackage = () => {
   const { detailId } = useParams();
   const { packages } = useGlobalContext();
 
-  // Convert `detailId` to a number if needed (assuming package IDs are numbers)
   const detailIdNumber = parseInt(detailId);
-
-  // Find the package by matching the ID
   const item = packages.find((item) => item.id === detailIdNumber);
 
   if (item) {
-    const { title, days,img } = item;
+    const { title, days, img } = item;
 
     return (
       <section>
-        <Navbar/>
-        <Hero 
-        cname="hero-mid"
-        heroImg={img[0]}
-        title={title}/>
+        <Navbar />
+        <Hero cname="hero-mid" heroImg={img[0]} title={title} />
 
-        {days.map(day=>{
-          return(
-            <article className='single-section'>
-              <div className="single">
-
-              <div className='iternary'>
-                <div className="iternary-sec">
-                <p>{day.title}</p>
-            <p>{day.desc}</p>
-            <p>{day.distance}</p>
-            <p>{day.hikingtime}</p>
-                </div>
-              
-              </div>
-              <div className='include-exclude'>
-<h3>kilimanjaro</h3>
-              </div>
-              </div>
-
-              
+        <div className="responsive-list">
+          <div className="list">
+             <ul className="list-style">
+            {days.map((day) => (
+              <li key={day.title}>
+                <h3>{day.title}</h3>
+                <p>{day.desc}</p>
+                <p>Distance:{day.distance}</p>
+                <p>Time:{day.hikingtime}</p>
+              </li>
+            ))}
+          </ul>
+          </div>
+          <div className="ex-in">
             
-            
-            
-            </article>
-            
-          )
-        })}
-        
+          </div>
+         
+        </div>
+        <Footer />
       </section>
     );
   } else {
     return (
       <section>
-        <p>Package not found.</p>
+        <p>Package not found</p>
       </section>
     );
   }
