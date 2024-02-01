965567a0cbe5ba2500421cd612d5988d5a7565d7
9mmasi <mmasiinnocent@gmail.com>
2024-02-01 10:04:06 +0300

    another day another push

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,6 +5,7 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import { WhatsAppIcon } from '../components/WhatsAppIcon'
 import AboutComp from '../components/AboutComp'
+import WhyUs from '../components/WhyUs'
 
 
 
@@ -21,6 +22,7 @@ const About = () => {
      
     />
     <AboutComp/>
+    <WhyUs/>
    
       <WhatsAppIcon />
       
